77845ff5199435e8d5aca2c095480c9417d3c0f1
Sotiris Morfakidis <sotirismorfakidis@gmail.com>
2023-11-10 23:58:54 +0200

    create scraper api token on startup

--- src/index.js
+++ src/index.js
@@ -1,5 +1,43 @@
 'use strict';
 
+async function createScraperTokenIfNotExist(strapi) {
+
+  const tokenService = strapi.service('admin::api-token');
+  if (tokenService && tokenService.create) {
+    const tokenAlreadyExists = await tokenService.exists({
+      name: 'scraper',
+    });
+    if (tokenAlreadyExists) {
+      console.info(`API token 'scraper' already exists, skipping...`);
+    }
+    else {
+      const token = await tokenService.create({
+        name: 'scraper',
+        lifespan: null,
+        type: 'custom',
+        permissions: [
+          "api::book.book.find",
+          "api::book.book.findOne",
+          "api::book.book.create",
+          "api::book.book.update",
+          "api::author.author.find",
+          "api::author.author.findOne",
+          "api::author.author.create",
+          "api::author.author.update",
+          "api::publisher.publisher.find",
+          "api::publisher.publisher.findOne",
+          "api::publisher.publisher.create",
+          "api::publisher.publisher.update",
+        ]
+      });
+      if (token.accessKey) {
+        console.info(`API token 'scraper' was created succesfully`);
+      }
+    }
+  }
+}
+
+
 module.exports = {
   /**
    * An asynchronous register function that runs before
@@ -7,7 +45,7 @@ module.exports = {
    *
    * This gives you an opportunity to extend code.
    */
-  register(/*{ strapi }*/) {},
+  register(/*{ strapi }*/) { },
 
   /**
    * An asynchronous bootstrap function that runs before
@@ -16,5 +54,7 @@ module.exports = {
    * This gives you an opportunity to set up your data model,
    * run jobs, or perform some special logic.
    */
-  bootstrap(/*{ strapi }*/) {},
+  async bootstrap({ strapi }) {
+    createScraperTokenIfNotExist(strapi);
+  },
 };
